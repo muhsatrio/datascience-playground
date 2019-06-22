4c0b9f649e91796cfb84dfd8804ed9c234805b13
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-06-22 18:15:09 +0700

    initial commit

--- dataCleaning/example6.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-import numpy as np 
-df = pd.DataFrame({'one': [10,20,30,40,50,2000],
-	'two':[1000,0,30,40,50,60]})
-print(df.replace({30:10,2000:60}))
--- dataOperations/example5.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import numpy as np 
-
-data = {
-	'Item1': pd.DataFrame(np.random.randn(4,3)),
-	'Item3': pd.DataFrame(np.random.randn(4,2))
-}
-p = pd.Panel(data)
-print(p)
